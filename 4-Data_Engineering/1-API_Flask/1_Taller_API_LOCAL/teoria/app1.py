e0b5108b7acd396c464652a92c1ca1a426363435
mrborjabarber <borja.barber@thebridgeschool.es>
2025-12-03 13:53:04 +0100

    w11d3

--- 4-Data_Engineering/1-API_Flask/1_Taller_API_LOCAL/teoria/app1.py
+++ 4-Data_Engineering/1-API_Flask/1_Taller_API_LOCAL/teoria/app1.py
@@ -5,5 +5,5 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Distant Reading Archive</h1><p>esto es para clase de data science.</p>"
 app.run()
